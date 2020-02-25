0155efb7281e3e9a2aa8589b7b0bdc44e0532690
MDimase <matias_dimase10@hotmail.com>
2020-02-25 17:14:41 -0300

    video 44

--- src/main/java/core/Alumno.java
+++ src/main/java/core/Alumno.java
@@ -1,10 +1,35 @@
 package core;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 public class Alumno {
-
+		
+		@NotBlank
+		@Size(min=2,message="campo requerido")
 		private String nombre;
+		
+		@NotBlank
+		@Size(min=2,message="campo requerido")
 		private String apellido;
+		
+		@NotNull
+		@Min(value=8,message="no se permiten menores de 8")
+		@Max(value=100,message="limite de edad 100")
+		private int edad;
+		
+		@NotBlank
+		@Email
+		private String email;
+		
+		@Pattern(regexp="[0-9]{5}",message="solo 5 valores numericos")
+		private String codigoPostal;
+		
 		private String optativa;
 		private String sede;
 		private boolean extranjero;
@@ -45,6 +70,24 @@ public class Alumno {
 		}
 		public void setIdioma(String idioma) {
 			this.idioma = idioma;
+		}
+		public int getEdad() {
+			return edad;
+		}
+		public void setEdad(int edad) {
+			this.edad = edad;
+		}
+		public String getEmail() {
+			return email;
+		}
+		public void setEmail(String email) {
+			this.email = email;
+		}
+		public String getCodigoPostal() {
+			return codigoPostal;
+		}
+		public void setCodigoPostal(String codigoPostal) {
+			this.codigoPostal = codigoPostal;
 		}
 		
 		
--- src/main/java/core/AlumnoController.java
+++ src/main/java/core/AlumnoController.java
@@ -1,8 +1,14 @@
 package core;
 
+import javax.validation.Valid;
+
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,16 +17,44 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/alumno")
 public class AlumnoController {
 	
+	@InitBinder
+	public void myBinder(WebDataBinder binder) {
+		//objeto encargado de tranformar una caja de texto con espacios en blancos a null
+		StringTrimmerEditor recorteBlancos = new StringTrimmerEditor(true);
+		
+		//creo el recorte de balncos
+		binder.registerCustomEditor(String.class, recorteBlancos);
+	}
+	
 	@GetMapping("/muestra")
 	public String muestraFormulario(Model model) {
 		Alumno alumno = new Alumno();
 		model.addAttribute("alumno",alumno);
 		return "alumnoRegistro";
 	}
-	
+	/*
 	@PostMapping("/alumnoRegistro")
 	public String alumnoRegistro(@ModelAttribute("alumno")Alumno alumno) {
 		System.out.println(alumno.getApellido()+alumno.getNombre()+alumno.getIdioma()+alumno.getOptativa()+alumno.getSede()+alumno.isExtranjero());
+		if(alumno.getIdioma() == null|| alumno.getSede() == null) {
+			alumno.setApellido(null);
+			alumno.setNombre(null);
+			alumno.setIdioma(null);
+			alumno.setOptativa(null);
+			alumno.setSede(null);
+			return "alumnoRegistro";
+		}
 		return "bienvenidoAlumno";
+	}
+	*/
+	@PostMapping("/alumnoRegistro")
+	public String alumnoRegistro(@Valid @ModelAttribute("alumno")Alumno alumno,BindingResult result) {
+		System.out.println(alumno.getApellido()+alumno.getNombre()+alumno.getIdioma()+alumno.getOptativa()+alumno.getSede()+alumno.isExtranjero()+alumno.getEdad()+alumno.getEmail());
+		if(result.hasErrors()) {
+			return "alumnoRegistro";
+		}else {
+			return "bienvenidoAlumno";
+		}
+		
 	}
 }
